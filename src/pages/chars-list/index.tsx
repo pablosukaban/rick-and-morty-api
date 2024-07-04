cffe2ea0ebd8c677dc54aecb643deece1cba7103
Ilya Prikhodko <zapaska96@gmail.com>
2024-07-04 19:49:44 +0500

    feat(charsList): добавил стили и наполнил контент для списка персонажей

--- src/pages/chars-list/index.tsx
+++ src/pages/chars-list/index.tsx
@@ -2,12 +2,19 @@ import { observer } from 'mobx-react-lite';
 import { useInstance } from 'react-ioc';
 import { CharsListStore } from './store';
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import {
+  Card,
+  Container,
+  Grid,
+  Typography,
+  CardContent,
+  CardMedia,
+  CardActions,
+  Button,
+} from '@mui/material';
 
 const CharsList = observer(() => {
   const store = useInstance(CharsListStore);
-
-  const navigate = useNavigate();
 
   useEffect(() => {
     store.init();
@@ -22,16 +29,42 @@ const CharsList = observer(() => {
   }
 
   return (
-    <div>
-      {store.fetchData.map((character) => (
-        <div
-          key={character.id}
-          onClick={() => navigate(`/character/${character.id}`)}
-        >
-          {character.name}
-        </div>
-      ))}
-    </div>
+    <Container maxWidth='md'>
+      <Typography variant='h2'>Персонажи</Typography>
+      
+      <Grid container spacing={2}>
+        {store.fetchData.map((character) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} key={character.id}>
+            <Card sx={{ height: '100%' }}>
+              <CardMedia
+                component='img'
+                height='140'
+                image={character.image}
+                alt={character.name}
+              />
+              <CardContent>
+                <Typography gutterBottom variant='h5' component='div'>
+                  {character.name}
+                </Typography>
+                <Typography variant='body2' color='text.secondary'>
+                  Status: {character.status}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button
+                  size='small'
+                  color='primary'
+                  variant='outlined'
+                  href={`/character/${character.id}`}
+                >
+                  Перейти
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 });
 
