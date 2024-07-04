f6b3a5155664a0d0145bdf1a4f1f5d5ece2aca7f
Ilya Prikhodko <zapaska96@gmail.com>
2024-07-04 19:26:12 +0500

    feat(Routing): добавил роутинг

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+const CharactersList = lazy(() => import('./chars-list/'));
+const CharactersItem = lazy(() => import('./chars-item/'));
+
+const Routing = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<CharactersList />} />
+      <Route path='/character/:id' element={<CharactersItem />} />
+    </Routes>
+  );
+};
+
+export default Routing;
