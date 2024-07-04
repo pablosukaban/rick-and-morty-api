66f61355aa19fef2979261a4a46a196f4f1d5cdc
Ilya Prikhodko <zapaska96@gmail.com>
2024-07-04 20:32:25 +0500

    feat(NetworkWrapper): добавил скелетоны в networkwrapper

--- src/shared/components/NetworkWrapper/index.tsx
+++ src/shared/components/NetworkWrapper/index.tsx
@@ -1,25 +1,38 @@
 import { Grid, Skeleton, Typography } from '@mui/material';
 import { ReactNode } from 'react';
 
+const SingleVariant = () => {
+  return <Skeleton variant='rectangular' height={140} />;
+};
+
+const MultipleVariant = () => {
+  return (
+    <Grid container spacing={2}>
+      {Array.from({ length: 20 }).map((_, i) => (
+        <Grid item xs={12} sm={6} md={4} lg={3} key={i}>
+          <Skeleton variant='rectangular' height={140} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
 const NetworkWrapper = ({
   children,
   isLoading,
   isEmpty,
+  variant,
 }: {
   isLoading: boolean;
   children: ReactNode;
   isEmpty: boolean;
+  variant?: 'single' | 'multiple';
 }) => {
+  const SkeletonComponent =
+    variant === 'single' ? SingleVariant : MultipleVariant;
+
   if (isLoading) {
-    return (
-      <Grid container spacing={2}>
-        {Array.from({ length: 20 }).map((_, i) => (
-          <Grid item xs={12} sm={6} md={4} lg={3} key={i}>
-            <Skeleton variant='rectangular' height={140} />
-          </Grid>
-        ))}
-      </Grid>
-    );
+    return <SkeletonComponent />;
   }
 
   if (!isLoading && isEmpty) {
